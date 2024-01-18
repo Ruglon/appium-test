7c48850c7dc0498677c8c8271e17cf234c0b3d87
vital <vitalicobalma@gmail.com>
2024-01-18 09:00:58 +0100

    Allure integration

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -9,9 +9,13 @@ import factory.DriverFactory;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import page.object.HomePage;
 import page.object.LoginPage;
 
+@Epic("Login Tests Epic")
+@Feature("Invalid Login Features")
 public class LoginTests {
 
     private AppiumDriver<MobileElement> driver;
@@ -33,8 +37,8 @@ public class LoginTests {
         DriverFactory.quitDriver();
     }
 
-    @Description("Login with valid data")
     @Test
+    @Description("Login with valid data")
     public void testLogin() {
         loginPage.enterUsername(ConfigReader.testConfig.login());
         loginPage.enterPassword(ConfigReader.testConfig.password());
@@ -42,8 +46,8 @@ public class LoginTests {
         homePage.checkHomePage();
     }
 
-    @Description("Sign in with invalid \"Login\" field")
     @Test
+    @Description("Sign in with invalid \"Login\" field")
     public void testLoginInvalid() throws InterruptedException {
         loginPage.enterUsername(ConfigReader.testConfig.illegalLogin());
         loginPage.enterPassword(ConfigReader.testConfig.password());
@@ -51,15 +55,15 @@ public class LoginTests {
         loginPage.assertInvalidDataMessage("Введены неверные данные");
     }
 
-    @Description("Illegal symbols not allowed for \"Login\" field")
     @Test
+    @Description("Illegal symbols not allowed for \"Login\" field")
     public void testIllegalSymbolsForLogin() {
         loginPage.enterUsername(ConfigReader.testConfig.invalidSymbolsLogin());
         loginPage.fieldIsEmpty("Login");
     }
 
-    @Description("Sign in with invalid \"Password\" field")
     @Test
+    @Description("Sign in with invalid \"Password\" field")
     public void testInvalidPassword() throws InterruptedException {
         loginPage.enterUsername(ConfigReader.testConfig.login());
         loginPage.enterPassword(ConfigReader.testConfig.illegalPass());
@@ -67,8 +71,8 @@ public class LoginTests {
         loginPage.assertInvalidDataMessage("Введены неверные данные");
     }
 
-    @Description("Illegal symbols not allowed for \"Password\" field")
     @Test
+    @Description("Illegal symbols not allowed for \"Password\" field")
     public void testIllegalSymbolsForPassword() {
         loginPage.enterPassword(ConfigReader.testConfig.invalidSymbolsLogin());
         loginPage.fieldIsEmpty("Password");
